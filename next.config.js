e1f2d62ee149408b801ff42d6e8422275040cc67
Shawnx0336 <shawn.davis2402@gmail.com>
2025-05-01 18:13:41 +0000

    Update config and frontend styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['http://137.184.104.153:3000'],
+  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.1.208:3000', 'http://137.184.104.153:3000'],
 };
 
 module.exports = nextConfig;
